d7f3882b88424a38532e6956bf4cecd556abc8de
wojak-frog <37157916+wojak-frog@users.noreply.github.com>
2021-02-22 22:08:54 +0200

    Update distribute_rewards.js

--- helper_scripts/distribute_rewards.js
+++ helper_scripts/distribute_rewards.js
@@ -1,25 +1,30 @@
 const jwk = `{}`
-const arweave = Arweave.init();
 const excluded_addresses = ["yjC9CFWNkQ6lGGm3n_gJwAu8c45yBc9e8dazXJWBN9M"];
 const reward_list = new Map();
 
 
+let rewards;
 
-async function usdToAr(usd) {
-	
-	
-	const ar_api = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=arweave&vs_currencies=usd");
-	const res = await ar_api.json();
-	const price = res["arweave"]["usd"];
+async function rewardsAmount() {
+	const fee_collector_balance = 
+		 await arweave.wallets.getBalance("BbODAb919DcZjX-50a2dzR1EvLK8zbGpr47bQikGCm4")
 
-	return usd / price
+		 console.log(fee_collector_balance)
+
+	rewards = fee_collector_balance / 2
+
+	return rewards
+
 
 };
 
 
-async function filterHolders(usd, distribution = usdToAr) {
+
+
+async function filterHolders() {
 	
-	  const ar_amount = await distribution(usd)
+	  const winston_amount = await rewardsAmount();
+
 	
       const state = await fetch("https://cache.community.xyz/contract/l4iqeiSb4oJrpByg6rgiXlW1iF3cgjXLbHdG2JvAC_c");
 
@@ -41,32 +46,33 @@ async function filterHolders(usd, distribution = usdToAr) {
      for ( let address of Object.keys(balancesObj) ) {
 
 
-     	let reward =  balancesObj[address] / totalHeldFqr  * ar_amount
+     	let reward =  balancesObj[address] / totalHeldFqr  * winston_amount
      	
      	reward_list.set(address, reward)
 
      }
     
-
      return reward_list
+     
 };
 
 
 
-async function distribute_rewards(usd) {
+async function distribute_rewards() {
 
-	const rewards = await filterHolders(usd)
+	const rewards = await filterHolders()
 
 	for ( holder of rewards.entries() ) {
 
 		let transaction = await arweave.createTransaction({
 			target: holder[0],
-			quantity: arweave.arToWinston( String(holder[1]) )
+			quantity: holder[1]
 
 		});
 
 		transaction.addTag("App-Name", "fQR-Weave");
-		transaction.addTag("description", "dividends distribution");
+		transaction.addTag("action", "dividends");
+		transaction.addTag("message", "Thank you for being a holder!")
 		
 		await arweave.transactions.sign(transaction, JSON.parse(jwk));
 		await arweave.transactions.post(transaction);
@@ -76,8 +82,4 @@ async function distribute_rewards(usd) {
 }
 
 
-distribute_rewards(usd_integer)
-
-
-
-
+distribute_rewards()
